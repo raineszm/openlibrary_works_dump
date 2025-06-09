0e8fbe0f5e9220ce5c8ddecde6d9cc9dbcdd9214
Zach Raines <dev@zmraines.com>
2025-06-08 21:08:22 -0500

    convert array properties into varchar arrays

--- parse_works.sql
+++ parse_works.sql
@@ -9,17 +9,17 @@ COPY (
         END as description,
         json_data->>'title' as title,
         json_data->>'subtitle' as subtitle,
-        json_data->'authors' as authors,
+        json_data->'authors'->>'$[*].author.key' as authors,
         json_data->>'location' as location,
         json_data->>'first_publish_date' as first_publish_date,
         json_data->'first_sentence'->>'value' as first_sentence,
         -- Classification arrays
-        json_data->'subjects' as subjects,
-        json_data->'subject_places' as subject_places,
-        json_data->'subject_people' as subject_people,
-        json_data->'subject_times' as subject_times,
-        json_data->'lc_classifications' as lc_classifications,
-        json_data->'dewey_number' as dewey_number
+        json_data->'subjects'->>'$[*]' as subjects,
+        json_data->'subject_places'->>'$[*]' as subject_places,
+        json_data->'subject_people'->>'$[*]' as subject_people,
+        json_data->'subject_times'->>'$[*]' as subject_times,
+        json_data->'lc_classifications'->>'$[*]' as lc_classifications,
+        json_data->'dewey_number'->>'$[*]' as dewey_number
     FROM (
             SELECT column4::JSON as json_data
             FROM read_csv_auto(
